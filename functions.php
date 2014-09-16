56e312857e6c632e86504c8e83b620f462fc10bd
root <karkochart@gmail.com>
2014-09-16 16:55:48 +0400

    added input functions

--- functions.php
+++ functions.php
@@ -21,15 +21,24 @@ function get_form_result(){
 	}
 	return($form_result);
 }
+function connect_DB(){
+
+	//connect DB
+	$connect=mysqli_connect("localhost","root","pas","Persons");
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	return $connect;
+}
+function close_DB(){
+
+	mysql_close(connect_DB());
+}
 function add_personal_date($name,$surname,$age){
 
 	if((preg_match('/^[A-Z][-a-zA-Z]+$/', $name))&&(preg_match('/^[A-Z][-a-zA-Z]+$/', $surname))&&(preg_match('/^[0-9,]{1,2}+$/', $age))){
-		//connect DB
-		$con=mysqli_connect("localhost","root","pas","Persons");
-		if (mysqli_connect_errno()) {
-			echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		}
 
+		$con = connect_DB();
 		$sql="INSERT INTO `Personal` ( `ID` , `Name` , `Surname` , `Age` )
 			VALUES ( NULL, '$name', '$surname', '$age' );";
 
@@ -37,5 +46,56 @@ function add_personal_date($name,$surname,$age){
 		if (!mysqli_query($con,$sql)) {
 			die('Error: ' . mysqli_error($con));
 		}
+		close_DB();
+		return true;
+	}else{
+		return false;
 	}
+
+}
+
+function get_id_by_name($table, $name, $surname="NULL", $age="NULL"){
+
+	$con = connect_DB();
+
+	if(($surname!="NULL") && ($age!="NULL")){
+		$surname_age = " and `Surname`='$surname' and `Age`='$age'" ;
+	}
+	$result = mysqli_query($con,"SELECT `ID` FROM `$table` WHERE `Name`='$name' ".$surname_age);
+
+	while($row = mysqli_fetch_array($result)) {
+		if($row["ID"]){
+			return $row["ID"];
+		}
+	}
+	close_DB();
+}
+
+function add_person_cat($pers_id, $cat_id){
+
+	$con = connect_DB();
+	$sql="INSERT INTO `State` ( `ID` , `Pers_ID` , `Cat_ID` ) VALUES ( NULL, '$pers_id', '$cat_id' );";
+
+	//Query DB
+	if (!mysqli_query($con,$sql)) {
+		die('Error: ' . mysqli_error($con));
+	}
+
+
+	close_DB();
+}
+
+
+function add_person_mark($pers_id, $subj_id,$mark){
+
+	$con = connect_DB();
+	$sql="INSERT INTO `Marks` ( `ID` , `Pers_ID` , `Subj_ID`, `Mark` ) VALUES ( NULL, '$pers_id', '$subj_id', '$mark' );";
+
+	//Query DB
+	if (!mysqli_query($con,$sql)) {
+		die('Error: ' . mysqli_error($con));
+	}
+
+
+	close_DB();
 }
--- index.php
+++ index.php
@@ -7,11 +7,13 @@
 <input type="text" name="surname"/>
 <input type="text" name="age"/>
 <select name="category">
+<option></option>
 <option>Employe</option>
 <option>Student</option>
 <option>Intern</option>
 </select>
 <select name="js">
+<option></option>
 <option>1</option>
 <option>2</option>
 <option>3</option>
@@ -24,6 +26,7 @@
 <option>10</option>
 </select>
 <select name="php">
+<option></option>
 <option>1</option>
 <option>2</option>
 <option>3</option>
@@ -40,8 +43,22 @@
 <?php
 require_once("functions.php"); 
 $add_person = get_form_result();
-add_personal_date($add_person["name"],$add_person["surname"],$add_person["age"]);
-//print_r($add_person);
+$person_added = add_personal_date($add_person["name"],$add_person["surname"],$add_person["age"]);
+
+
+if($person_added){
+
+	$person_id = get_id_by_name('Personal', $add_person["name"], $add_person["surname"], $add_person["age"]);
+	$subjects = array('js' => $add_person['js'], 'php' => $add_person['php']);
+	foreach($subjects as $key => $val){
+		if($val){
+			add_person_mark($person_id, get_id_by_name('Subjects', $key) , $val);
+		}
+	}
+	if($add_person["category"]){
+		add_person_cat($person_id , get_id_by_name('Categories', $add_person["category"]));
+	}
+}
 ?>
 
 </body>
